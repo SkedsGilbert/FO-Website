815a35ba950910851c7e11239105150ac61ceb36
Jsin <jsin1947@gmail.com>
2015-07-06 21:52:28 -0500

    Setup file structure, Created DB config file, and DB connection file

---
+++ www/family_organizer/include/DbConnect.php
@@ -0,0 +1,23 @@
+<?php
+
+Class DbConnect{
+	private $conn;
+
+	function _construct(){
+
+	}
+
+	function connect(){
+		inclued_once dirname(_FILE_).'./Config.php';
+
+		//DB connection
+		$this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+		//Check for connection
+		if (mysql_errno()) {
+			echo "Failed to connect to MySQL: ".mysql_error();
+		}
+		return $this->conn;
+	}
+}
+
+?>
